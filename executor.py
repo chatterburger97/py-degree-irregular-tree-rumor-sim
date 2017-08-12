f0ac3fbbc49cca5b11baa4a6219d8fb5806112e8
chatterburger97 <paromac@gmail.com>
2017-08-13 04:55:44 +0800

    Simulations run on same data, syntax corrections

--- executor.py
+++ executor.py
@@ -1,46 +1,47 @@
 import search_heuristics as srch
 from dijkstra import dijkstra
 import matplotlib.pyplot as plt
-import hashlib
-import time
 
-def findMaxPossibilityPath(chosen_search, graph, infected_group, bfs_argument=1) :
-    # max probability among the probabilities of any of the permutations that result in infected_group
+
+def findMaxPossibilityPath(chosen_search, input_graph, infected_group, bfs_argument=1):
+    # max probability among the probabilities of any of
+    # the permutations that result in infected_group
     max_permutation_probability = 0
     # the node with the largest estimated probability as found above
     estimated_start_node = 0
-   
-    if chosen_search == 'bfs' : 
-        for val in infected_group :
-            possibility = graph.bfs(val, bfs_argument)[1]
-            if max_permutation_probability < float(possibility):
-                max_permutation_probability = float(possibility)
+    if chosen_search == 'bfs':
+        for val in infected_group:
+            adjusted_possibility = input_graph.rumor_centrality(val) * input_graph.bfs(val, bfs_argument)[1]
+            if max_permutation_probability < float(adjusted_possibility):
+                max_permutation_probability = float(adjusted_possibility)
                 estimated_start_node = val
     elif chosen_search == 'min_deg_search':
-        for val in infected_group :
-            possibility = graph.min_deg_search(val)[1]
-            if max_permutation_probability < float(possibility):
-                max_permutation_probability = float(possibility)
+        for val in infected_group:
+            adjusted_possibility = input_graph.rumor_centrality(val) * input_graph.min_deg_search(val)[1]
+            if max_permutation_probability < float(adjusted_possibility):
+                max_permutation_probability = float(adjusted_possibility)
                 estimated_start_node = val
-    elif chosen_search == 'max_deg_search' :
-        for val in infected_group :
-            possibility = graph.max_deg_search(val)[1]
-            if max_permutation_probability < float(possibility):
-                max_permutation_probability = float(possibility)
+    elif chosen_search == 'max_deg_search':
+        for val in infected_group:
+            adjusted_possibility = input_graph.rumor_centrality(val) * input_graph.max_deg_search(val)[1]
+            if max_permutation_probability < float(adjusted_possibility):
+                max_permutation_probability = float(adjusted_possibility)
                 estimated_start_node = val
-    elif chosen_search == 'dfs' :
-        for val in infected_group :
-            possibility = graph.dfs(val)[1]
-            if max_permutation_probability < float(possibility):
-                max_permutation_probability = float(possibility)
+    elif chosen_search == 'dfs':
+        for val in infected_group:
+            adjusted_possibility = input_graph.rumor_centrality(val) * input_graph.dfs(val)[1]
+            if max_permutation_probability < float(adjusted_possibility):
+                max_permutation_probability = float(adjusted_possibility)
                 estimated_start_node = val
-    else :
+    else:
         print 'please refer to search_heuristics for a list of valid search methods'
-        
+
     return estimated_start_node
 
-class simulation_executor :
-    def __init__(self, chosen_search, sim_count, bfs_argument = 1) :
+
+class simulation_executor:
+    def __init__(self, input_graph, chosen_search, sim_count, bfs_argument=1):
+        self.input_graph = input_graph
         self.chosen_search = chosen_search
         self.sim_count = sim_count
         self.bfs_argument = bfs_argument
@@ -49,62 +50,66 @@ class simulation_executor :
         self.iteration = 1
         print 'initialised new simulator'
 
-    def simulate(self) :
+    def simulate(self):
         print 'entering simulate function with search '
         for val in range(0, self.sim_count, 1):
-            graph = srch.Graph(100, 3)
-            constructed_graph = graph.return_full_underlying_graph()
-            try :
-                source, infected_group = graph.spread_rumor(10)
+            constructed_graph = self.input_graph.return_full_underlying_graph()
+            try:
+                source, infected_group = self.input_graph.spread_rumor(10)
             except ValueError:
-                self.sim_count-=1
-                continue;
+                self.sim_count -= 1
+                continue
             else:
                 dist_map = dict()
                 dijkstra(constructed_graph, infected_group, dist_map, source)
-                estimated_start_node = findMaxPossibilityPath(self.chosen_search, graph, infected_group, self.bfs_argument) 
-                print 'estimated start node : ', estimated_start_node
+                estimated_start_node = findMaxPossibilityPath(self.chosen_search, self.input_graph, infected_group, self.bfs_argument)
+                print 'estimated start node: ', estimated_start_node
                 print 'hop error: ', dist_map[estimated_start_node]
                 self.y_points.append(dist_map[estimated_start_node])
-                print 'iteration number : ', val+1
-                self.x_points.append(val + 1) # iteration number
-    
-    def plot_simulation(self, file_prefix=None) :
+                print 'iteration number: ', val + 1
+                self.x_points.append(val + 1)  # iteration number
+
+    def plot_simulation(self, file_prefix=None):
         print '<plotting simulation>\n'
-        plt.plot(self.x_points,self.y_points, 'ro')
+        plt.plot(self.x_points, self.y_points, 'ro')
         plt.xlabel('Iteration number')
         plt.ylabel('Hop error b/w estimated best probability node and actual rumor source')
-        if file_prefix is None :
+        if file_prefix is None:
             plt.show()
-        else :
-            filename = self.chosen_search + '_'+ file_prefix + '_' + str(self.iteration) + '.png'
+        else:
+            filename = self.chosen_search + '_' + file_prefix + '_' + str(self.iteration) + '.png'
             plt.savefig(str(filename))
-            print 'filename : ', filename
-            self.iteration+=1
+            print 'filename: ', filename
+            self.iteration += 1
 
-def plotAll(sim_count, file_prefix) :
+
+def plotAll(input_graph, sim_count, file_prefix):
     # plot for natural bfs
     # natural_bfs_sim  = simulation_executor('bfs', sim_count)
     # natural_bfs_sim.simulate()
-    # natural_bfs_sim.plot_simulation(file_prefix)
 
     # plot for ascending bfs
     # ascending_bfs_sim = simulation_executor('bfs',sim_count, 2)
     # ascending_bfs_sim.simulate()
-    # ascending_bfs_sim.plot_simulation(file_prefix)
 
     # plot for descending
-    descending_bfs_sim = simulation_executor('bfs', sim_count, 3)
+    descending_bfs_sim = simulation_executor(input_graph, 'bfs', sim_count, 3)
     descending_bfs_sim.simulate()
-    descending_bfs_sim.plot_simulation(file_prefix)
 
     # # plot for min_degree
     # min_deg_sim = simulation_executor('min_deg_search',3)
-    # min_deg_sim.plot_simulation()
 
     # # plot for max degree
     # max_deg_sim = simulation_executor('max_deg_search', 3)
+
+    # natural_bfs_sim.plot_simulation(file_prefix)
+    # ascending_bfs_sim.plot_simulation(file_prefix)
+    descending_bfs_sim.plot_simulation(file_prefix)
+    # min_deg_sim.plot_simulation()
     # max_deg_sim.plot_simulation()
+
+
+input_graph = srch.Graph(100, 3)
 user_defined_sim_count = raw_input("Please enter the number of times you wish to simulate\n")
 user_defined_names = raw_input("Enter a unique name for the saved plot file \n")
-plotAll(int(user_defined_sim_count), user_defined_names)
+plotAll(input_graph, int(user_defined_sim_count), user_defined_names)
--- priority_queue.py
+++ priority_queue.py
@@ -1,45 +1,46 @@
 from heapq import *
 import itertools
 
-class PriorityQueue : 
 
-	REMOVED = '<removed-task>'      # placeholder for a removed task
-	
-	def __init__(self, argument_map=dict()) :
-		self.pq = []				# list of entries arranged in a heap
-		self.entry_finder = {}		# mapping of tasks to entries
-		self.counter = itertools.count()     # unique sequence count
-		for val in argument_map.keys() :
-			self.add_task(val, argument_map[val])
+class PriorityQueue:
 
-	def add_task(self, task, priority=0):
-	    # 'Add a new task or update the priority of an existing task'
-	    if task in self.entry_finder:
-	        self.remove_task(task)
-	    count = next(self.counter)
-	    entry = [priority, count, task]
-	    self.entry_finder[task] = entry
-	    heappush(self.pq, entry)
+    REMOVED = '<removed-task>'      # placeholder for a removed task
 
-	def remove_task(self, task):
-	    # 'Mark an existing task as REMOVED.  Raise KeyError if not found.'
-	    entry = self.entry_finder.pop(task)
-	    entry[-1] = PriorityQueue.REMOVED
+    def __init__(self, argument_map=dict()):
+        self.pq = []                # list of entries arranged in a heap
+        self.entry_finder = {}      # mapping of tasks to entries
+        self.counter = itertools.count()     # unique sequence count
+        for val in argument_map.keys():
+            self.add_task(val, argument_map[val])
 
-	def pop_task(self):
-	    # 'Remove and return the lowest priority task. Raise KeyError if empty.'
-	    while self.pq:
-	        priority, count, task = heappop(self.pq)
-	        if task is not PriorityQueue.REMOVED:
-	            del self.entry_finder[task]
-	            return task
-	    raise KeyError('pop from an empty priority queue')
+    def add_task(self, task, priority=0):
+        # 'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
+            self.remove_task(task)
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heappush(self.pq, entry)
 
-	def peek_min(self):
-	    # 'Remove and return the lowest priority task. Raise KeyError if empty.'
-	    while self.pq:
-	        priority, count, task = heappop(self.pq)
-	        if task is not REMOVED:
-	            del self.entry_finder[task]
-	            return task
-	    raise KeyError('pop from an empty priority queue')
+    def remove_task(self, task):
+        # 'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = PriorityQueue.REMOVED
+
+    def pop_task(self):
+        # 'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heappop(self.pq)
+            if task is not PriorityQueue.REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
+
+    def peek_min(self):
+        # 'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heappop(self.pq)
+            if task is not REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
